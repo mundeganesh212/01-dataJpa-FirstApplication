72f736eca90a3adb663513899ed6bb6ae1fd025c
Vikas5591 <viaksmadhukar.60@gmail.com>
2021-09-27 23:01:23 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,10 @@
+/*
+			 * UserEntity u = repo.getUserByUserName1("bikkad"); System.out.println(u);
+			 */
+		   
+		     List <UserEntity> list =repo.getUserByEmail1("mundeganu212@gmail.com");
+		     
+		     for(UserEntity u:list) {
+		    	 
+		    	 System.out.println(u);
+		     }
